4794b6bba6178ccc2ceed2cd8c578be6ecc821ab
Антон <antonolegovich2017@icloud.com>
2023-06-05 20:13:59 +0600

    вя

---
+++ src/main/java/com/example/demoanimalbot/model/Cat.java
@@ -0,0 +1,36 @@
+package com.example.demoanimalbot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+public class Cat extends Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private int age;
+    private String breed;
+    private String description;
+    private Status status;
+    @ManyToOne
+    private UserCat user;
+
+    public Cat(String name, int age, String breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        this.status = Status.SHELTER;
+    }
+
+
+}
