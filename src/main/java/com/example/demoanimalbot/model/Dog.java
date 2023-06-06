a850dd1369619151db9e15249756c0c5c0869d0f
Nikolay <razenkov.rna@mail.ru>
2023-06-06 11:08:24 -0700

    CatUserController

---
+++ src/main/java/com/example/demoanimalbot/model/Dog.java
@@ -0,0 +1,37 @@
+package com.example.demoanimalbot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+public class Dog extends Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private int age;
+    private String breed;
+    private String description;
+    private Status status;
+    private LocalDate dateOfAdoption;
+
+    @ManyToOne
+    private UserDog user;
+
+    public Dog(String name, int age, String breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        this.status = Status.SHELTER;
+    }
+}
